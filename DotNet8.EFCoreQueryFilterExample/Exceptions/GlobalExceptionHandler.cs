9a2599be885f277417ac39665d79a23fa9b3ef6a
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-12 11:46:45 +0630

    -

--- DotNet8.EFCoreQueryFilterExample/Exceptions/GlobalExceptionHandler.cs
+++ DotNet8.EFCoreQueryFilterExample/Exceptions/GlobalExceptionHandler.cs
@@ -2,18 +2,21 @@
 
 public class GlobalExceptionHandler : IExceptionHandler
 {
-    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken
+    )
     {
         var problemDetails = new
         {
             Status = StatusCodes.Status500InternalServerError,
             Title = "Server error"
-        }; 
-        
+        };
+
         httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
 
-        await httpContext.Response
-            .WriteAsJsonAsync(problemDetails, cancellationToken);
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
 
         return true;
     }
