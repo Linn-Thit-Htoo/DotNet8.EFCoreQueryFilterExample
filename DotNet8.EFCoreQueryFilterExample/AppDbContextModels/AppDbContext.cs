623b95dcab225c1c9721830692cb9e85238aa074
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-12 11:44:15 +0630

    -

--- DotNet8.EFCoreQueryFilterExample/AppDbContextModels/AppDbContext.cs
+++ DotNet8.EFCoreQueryFilterExample/AppDbContextModels/AppDbContext.cs
@@ -4,9 +4,8 @@ namespace DotNet8.EFCoreQueryFilterExample.AppDbContextModels;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions options) : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions options)
+        : base(options) { }
 
     public DbSet<Tbl_Blog> Tbl_Blog { get; set; }
 
