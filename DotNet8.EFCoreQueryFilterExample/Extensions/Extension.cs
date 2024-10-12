f1a3bca4e1299e35a292af4ff9922516f871ad21
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-12 11:47:09 +0630

    -

--- DotNet8.EFCoreQueryFilterExample/Extensions/Extension.cs
+++ DotNet8.EFCoreQueryFilterExample/Extensions/Extension.cs
@@ -2,7 +2,8 @@
 
 public static class Extension
 {
-    public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj, Formatting.Indented);
+    public static string ToJson(this object obj) =>
+        JsonConvert.SerializeObject(obj, Formatting.Indented);
 
     public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
 }
